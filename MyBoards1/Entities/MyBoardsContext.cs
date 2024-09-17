9136c3fb979a50f5d2fe8a4e6bb9599544986b8d
SzymekNawrocki <129281704+SzymekNawrocki@users.noreply.github.com>
2024-09-17 12:26:27 +0200

    Entity Framework

---
+++ MyBoards1/Entities/MyBoardsContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace MyBoards.Entities
+{
+    public class MyBoardsContext : DbContext
+    {
+        public MyBoardsContext(DbContextOptions<MyBoardsContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<WorkItem> WorkItems { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<WorkItem>(eb =>
+            {
+                eb.Property(x => x.State).IsRequired();
+                eb.Property(x => x.Area).HasColumnType("varchar(200)");
+                eb.Property(wi => wi.IterationPath).HasColumnName("Iteration_Path");
+                eb.Property(wi => wi.Efford).HasColumnType("decimal(5,2)");
+                eb.Property(wi => wi.EndDate).HasPrecision(3);
+                eb.Property(wi => wi.Activity).HasMaxLength(200);
+                eb.Property(wi => wi.RemainingWork).HasPrecision(14, 2);
+                eb.Property(wi => wi.Priority).HasDefaultValue(1);
+            });
+
+            modelBuilder.Entity<Comment>(eb =>
+            {
+                eb.Property(x => x.CreatedDate).HasDefaultValueSql("getutcdate()");
+                eb.Property(x => x.UpdatedDate).ValueGeneratedOnUpdate();
+            });
+        }
+
+    }
+}
